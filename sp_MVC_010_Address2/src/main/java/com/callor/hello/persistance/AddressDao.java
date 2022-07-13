c33be8ba2a92fa75c855cf0bf4b716ab76833d6c
ppunikim <yd62322@naver.com>
2022-07-13 16:21:45 +0900

    데이터베이스 연동 전편

--- sp_MVC_010_Address2/src/main/java/com/callor/hello/persistance/AddressDao.java
+++ sp_MVC_010_Address2/src/main/java/com/callor/hello/persistance/AddressDao.java
@@ -1,5 +1,17 @@
 package com.callor.hello.persistance;
 
-public class AddressDao {
+import java.util.List;
 
+import com.callor.hello.model.AddressVO;
+
+public interface AddressDao {
+
+	public List<AddressVO> selectAll();
+	public AddressVO findById(long ad_seq);
+	public int insert(AddressVO vo);
+	public int update(AddressVO vo);
+	public int delete(AddressVO vo);
+	public List<AddressVO> findByName(String name);
+	public List<AddressVO> findByTel(String tel);
+	public List<AddressVO> findByAddr(String addr);
 }
